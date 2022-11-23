5ab30d447e0f330b2522484612066e8059c43bfb
raininsnovv <raininsnovv@gmail.com>
2022-11-23 17:27:23 +0300

    Correct css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ const Card = (props) => {
   return (
     <div className="card">
       <img className="img" src={props.img} alt="" />
-      <div className="name">Наименование товара - {props.name}</div>
-      <div className="price">Цена : {props.price} рублей</div>
-      <div className="left">Осталось в наличии : {props.left} штук</div>
-      <div className="id">Код товара : {props.id}</div>
+      <div className="name">Наименование товара: {props.name}</div>
+      <div className="price">Цена: {props.price} рублей</div>
+      <div className="left">Осталось в наличии: {props.left} штук</div>
+      <div className="id">Код товара: {props.id}</div>
     </div>
   );
 };
